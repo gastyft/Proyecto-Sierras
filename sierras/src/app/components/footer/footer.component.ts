48396899e843bfa40528b66caf7cfc7e1b11d6c5
gastyft <gastyftacla1@hotmail.com>
2024-01-26 00:04:30 -0300

    correcion del mensaje del footer

--- sierras/src/app/components/footer/footer.component.ts
+++ sierras/src/app/components/footer/footer.component.ts
@@ -11,7 +11,7 @@ export class FooterComponent {
   enviarMensaje() {
     // Número de teléfono y mensaje predeterminado
     const numeroTelefono = '+5492235443274'; // Reemplaza con tu número de teléfono
-    const mensaje = 'Hola, estoy interesado en tu en averiguar por la estadía';
+    const mensaje = 'Hola, estoy interesado en averiguar por la estadía';
 
     // Construir el enlace de WhatsApp con el mensaje
     const enlaceWhatsApp = 'https://wa.me/' + numeroTelefono + '?text=' + encodeURIComponent(mensaje);
